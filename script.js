5462e43fb23142fc68a38192fb4c6e939aacab91
Husnain Syed <69011172+DaimSyed@users.noreply.github.com>
2021-02-11 00:41:18 -1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", () =>{
+    search.classList.toggle("active");
+    input.focus();
+})
